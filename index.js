ed17652ca698eb0c1f7a2ed40baaf9a7c2f2ee20
sathizzkumar <kumarsathish09354@gmail.com>
2025-10-31 19:48:28 +0530

    postgres install

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const cors = require('cors');
 const app = express();
 const port = 8000;
 
+const { Client } = require('pg')
+
+const con = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'carequality',
+  password: 'admin',
+  port: 5432,
+});
+
+con.connect().then(() => {
+  console.log("✅ Connected to PostgreSQL database");
+}).catch(err => {
+  console.error("❌ Connection to PostgreSQL database failed:", err);
+});
+
+con.query("select * from Organization_Data", (err, res) => {
+  if (!err) {
+    console.log("✅ Query Result:", res.rows);
+  } else {
+    console.error("❌ Query Error:", err);
+  }
+})
+
 const router = require('./Router/router');
 
 app.use(bodyParser.json({ limit: '50mb' })); 
